057a981419e246a58a11feee6d11dc327c3030df
Ahmed Akmal <akmal.ahmed525@gmail.com>
2018-05-25 16:01:07 +0530

    Update CRUD.js

--- src/api/CRUD.js
+++ src/api/CRUD.js
@@ -6,23 +6,23 @@ class CRUD {
     insertData(title, date, note, token){
         let id = IDGenerator()
         let data={id, title, date, note, token}
-        return axios.post('http://localhost:5500/api/profile/i', data)
+        return axios.post('http://localhost:xxxx/api/profile/i', data)
         
     }
 
     readData(token){
-        return axios.post('http://localhost:5500/api/profile/r', {token})
+        return axios.post('http://localhost:xxxx/api/profile/r', {token})
     }
 
     editData(id ,title, date, note, token){
         let data={id, title, date, note, token}
         console.log(data)
-        return axios.post('http://localhost:5500/api/profile/e', data)
+        return axios.post('http://localhost:xxxx/api/profile/e', data)
     }
 
     deleteData(id, token){
         let data = {id, token}
-        return axios.post('http://localhost:5500/api/profile/d', data)
+        return axios.post('http://localhost:xxxx/api/profile/d', data)
     }
 }
 
