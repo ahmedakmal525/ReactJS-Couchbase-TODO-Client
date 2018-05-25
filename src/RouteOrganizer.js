8b165e412329470947afcb91f5b96eced8161a06
Ahmed Akmal <akmal.ahmed525@gmail.com>
2018-05-25 15:58:50 +0530

    Update RouteOrganizer.js

--- src/RouteOrganizer.js
+++ src/RouteOrganizer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Route from 'react-router-dom/Route'
 import Login from './components/Login'
 import Profile from './components/Profile'
